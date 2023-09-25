89c3149b02afadb51aaf81b9c3fc43000f5ecb1e
Jingning <seamissu@gmail.com>
2023-09-25 20:26:45 +1000

    Finish Shell and Sneaker Grid

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -24,26 +24,57 @@ const ShoeCard = ({
   // on-sale. In theory, it is possible for a shoe to be
   // both on-sale and new-release, but in this case, `on-sale`
   // will triumph and be the variant used.
-  // prettier-ignore
-  const variant = typeof salePrice === 'number'
-    ? 'on-sale'
-    : isNewShoe(releaseDate)
+
+  const variant =
+    typeof salePrice === 'number'
+      ? 'on-sale'
+      : isNewShoe(releaseDate)
       ? 'new-release'
-      : 'default'
+      : 'default';
+
+  const FLAGS = {
+    'on-sale': {
+      '--width': 49 + 'px',
+      '--height': 32 + 'px',
+      '--color': `${COLORS.white}`,
+      '--backgroundColor': `${COLORS.primary}`,
+      '--fontSize': 14 + 'px',
+      '--fontWeight': `${WEIGHTS.medium}`,
+      text: 'Sale',
+    },
+    'new-release': {
+      '--width': 118 + 'px',
+      '--height': 32 + 'px',
+      '--color': `${COLORS.white}`,
+      '--backgroundColor': `${COLORS.secondary}`,
+      '--fontSize': 14 + 'px',
+      '--fontWeight': `${WEIGHTS.medium}`,
+      text: 'Just Released!',
+    },
+    default: {
+      text: '',
+    },
+  };
 
   return (
     <Link href={`/shoe/${slug}`}>
       <Wrapper>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
+          <Flag style={FLAGS[variant]}>{FLAGS[variant].text}</Flag>
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price isOnSale={variant === 'on-sale'}>
+            {formatPrice(price)}
+          </Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          <SalePrice>
+            {salePrice ? formatPrice(salePrice) : undefined}
+          </SalePrice>
         </Row>
       </Wrapper>
     </Link>
@@ -53,6 +84,7 @@ const ShoeCard = ({
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+  flex: 1 1 275px;
 `;
 
 const Wrapper = styled.article``;
@@ -61,10 +93,30 @@ const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+  border-radius: 16px 16px 4px 4px;
+`;
+
+const Flag = styled.div`
+  position: absolute;
+  top: 12px;
+  right: -6px;
+  width: var(--width);
+  height: var(--height);
+  color: var(--color);
+  background-color: var(--backgroundColor);
+  font-size: var(--fontSize);
+  font-weight: var(--fontWeight);
+  border-radius: 2px;
+  padding: 6px 10px;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
 `;
 
 const Name = styled.h3`
@@ -72,7 +124,10 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  text-decoration: ${(prop) =>
+    prop.isOnSale ? 'line-through' : undefined};
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -25,7 +25,7 @@ const ShoeIndex = ({ sortId, setSortId }) => {
           </Select>
         </Header>
         <Spacer size={34} />
-        {/* <ShoeGrid />  */}
+        <ShoeGrid />
       </MainColumn>
       <LeftColumn>
         <Breadcrumbs>
@@ -46,11 +46,13 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: row-reverse;
   justify-content: flex-end;
-  gap: 128px;
+  gap: 32px;
   align-items: baseline;
 `;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+  width: 248px;
+`;
 
 const MainColumn = styled.div`
   flex: 1 1 0;
